f36b769cdecd6e42f93ae7658d4287bcf5871417
murilopereirame <34790144+murilopereirame@users.noreply.github.com>
2018-05-07 21:54:35 -0300

    Update Divisores.c

--- Divisores.c
+++ Divisores.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <locale.h>
 
 int main(void){
-
+    //Encontre seus divisores de um nÃºmero inteiro positivo num e a quantidade deles;
+    
     setlocale(LC_ALL,"");
 
     int num, qnt;
 
-    printf("Insira um número inteiro positivo: ");
+    printf("Insira um nÃºmero inteiro positivo: ");
     scanf("%d", &num);
 
     for(int i=1; i <= num; i++){
         if(num%i == 0){
             qnt++;
-            printf("\n%d é divisor de %d\n", i, num);
+            printf("\n%d Ã© divisor de %d\n", i, num);
         }
     }
 
