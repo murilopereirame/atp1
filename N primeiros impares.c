642ff94faa19fffa69c1b4e6327b0851728fa7d6
murilopereirame <34790144+murilopereirame@users.noreply.github.com>
2018-05-07 21:52:32 -0300

    Update N primeiros impares.c

--- N primeiros impares.c
+++ N primeiros impares.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <locale.h>
 
 #define LIM 100000
 
 int main(void){
+    //Imprimir os n primeiros nÃºmeros Ã­mpares;
+    
     setlocale(LC_ALL,"");
 
     int n, qnt=0;
 
-    printf("Insira a quantidade de números impares desejados: ");
+    printf("Insira a quantidade de nÃºmeros impares desejados: ");
     scanf("%d", &n);
 
     for(int i=0; i <= LIM; i++){
         if(!(i%2 == 0)){
-            printf("\nO número %d é ímpar!\n", i);
+            printf("\nO nÃºmero %d Ã© Ã­mpar!\n", i);
             qnt++;
         }
         if(qnt == n)
